3b1a952b644de4a0983f51fae0e91dfdc561b00e
iggiond <117941262+iggiond@users.noreply.github.com>
2022-11-10 20:11:47 +0100

    Update Challenge45.kt

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge45.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge45.kt
@@ -29,5 +29,35 @@ package com.mouredev.weeklychallenge2022
  * - Tienes toda la información sobre los retos semanales en
  *   https://retosdeprogramacion.com/semanales2022.
  *
+ *
+ *
+ *Llevo solo una semana programando y es mi primer reto, cualquier critica se agradece
  */
 
+array = (4, 0, 3, 6, 1, 3)
+
+n = array[0]
+total = 0
+valor = len(array)
+x = 0
+
+for i in range(valor):
+    try:
+        indice = array[x]
+        resultado= n - indice
+        x+=1
+        maximo = max(array[x:])
+        if maximo >= n > indice:
+            resultado
+            total = total+resultado
+        elif n > maximo > indice:
+            resultado_alternativo = maximo - indice
+            total = total+resultado_alternativo
+        elif n <= indice:
+            n = indice
+        elif maximo <= indice:
+            continue
+    except ValueError:
+        break      
+        
+print(f'En total hay {total} gotas de agua')
